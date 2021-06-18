5b6535ef1d1faba11e62555ad43962d673e9a573
lzpel <18492524+lzpel@users.noreply.github.com>
2021-06-18 18:08:42 +0900

    add len

--- blocklyDemo/src/App.js
+++ blocklyDemo/src/App.js
@@ -4,9 +4,26 @@ import './App.css';
 import Blockly from 'blockly';
 
 function App() {
-
     React.useEffect(() => {
-        Blockly.inject('blocklyDiv', {
+        Blockly.Blocks['string_length'] = {
+            init: function() {
+                this.jsonInit({
+                    "message0": '%1 の長さ',
+                    "args0": [
+                        {
+                            "type": "input_value",
+                            "name": "VALUE",
+                            "check": "String"
+                        }
+                    ],
+                    "output": "Number",
+                    "colour": 160,
+                    "tooltip": "Returns number of letters in the provided text.",
+                    "helpUrl": "http://www.w3schools.com/jsref/jsref_length_string.asp"
+                });
+            }
+        };
+        let workspace = Blockly.inject('blocklyDiv', {
             toolbox: document.getElementById('toolbox')
         });
     });
@@ -15,13 +32,18 @@ function App() {
             <h1>blockly demo</h1>
             <div id="blocklyDiv" style={{height: "480px", width: "600px"}}></div>
             <xml id="toolbox" style={{display: "none"}}>
-                <block type="controls_if"></block>
-                <block type="controls_repeat_ext"></block>
-                <block type="logic_compare"></block>
-                <block type="math_number"></block>
-                <block type="math_arithmetic"></block>
-                <block type="text"></block>
-                <block type="text_print"></block>
+                <category name="既存部品">
+                    <block type="controls_if"></block>
+                    <block type="controls_repeat_ext"></block>
+                    <block type="logic_compare"></block>
+                    <block type="math_number"></block>
+                    <block type="math_arithmetic"></block>
+                    <block type="text"></block>
+                    <block type="text_print"></block>
+                </category>
+                <category name="カスタム部品">
+                    <block type="string_length"></block>
+                </category>
             </xml>
         </div>
     );
